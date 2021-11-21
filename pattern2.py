12fa9fc91bb42e2ce0825b5553035a893411f27b
Gunjankushwaha <94755921+Gunjankushwaha@users.noreply.github.com>
2021-11-21 21:08:30 +0530

    Add files via upload

---
+++ pattern2.py
@@ -0,0 +1,159 @@
+# *
+# **
+# ***
+# ****
+# *****
+# i=1                                                        
+# while i<=5:
+#    j=1
+#    while j<=i:
+#       print("*",end="")
+#       j=j+1
+#    print()
+#    i=i+1
+
+
+
+
+
+#     *
+#    **
+#   ***
+#  ****
+# *****
+# i=5
+# while i<=5:
+#     b=1
+#     while b<=i-1:
+#         print(" ",end="")
+#         b=b+1
+#     j=5
+#     while j>=1:
+#         print("*",end="")
+#         j=j-1
+#     print()
+#     i=i-1
+
+
+
+#      *
+#     ***
+#    *****
+#   *******
+#  *********
+
+# k=1
+# i=1
+# while i<=5:
+#     b=1
+#     while b<=5-i:                                                              
+#         print("-",end="")
+#         b=b+1
+#     j=1
+#     while j<=k:
+#         print("*",end="")
+#         j=j+1
+#     k=k+2
+#     print()
+#     i=i+1
+
+# *********
+#  *******
+#   *****
+#    ***
+#     * 
+# n=int(input("enter the number"))
+# i=1
+# while (n>0):
+#     b=1
+#     while (b<i):
+#         print(" ",end="")
+#         b=b+1
+#     j=1
+#     while j<=(n*2-1):
+#         print("*",end="")
+#         j=j+1
+#     print()
+#     n=n-1
+#     i=i+1
+
+
+
+
+    
+
+
+# diamond
+#       *
+#      ***
+#     *****
+#    *******
+#   *********
+#    *******
+#     *****
+#      ***
+#       *
+
+# i= 1
+# while (i<=9):
+#     b=1
+#     while (b<=9-i):                                          
+#         print(" ",end="") 
+#         b=b+1
+#     j=1
+#     while (j<=i*2-1):
+#         print("*",end="")
+#         j=j+1
+#     print()
+#     i=i+1
+# i=9
+# while (i>=1):
+#     b=1
+#     while (b<=9-i):
+#         print(" ",end="")
+#         b=b+1
+#     j=1
+#     while (j<=2*i-1):
+#         print("*",end="")
+#         j=j+1
+#     print()
+#     i=i-1
+
+
+
+# for i in range(6):
+#     for a in range(7):
+#         if (i==0 and a%3!=0) or (i==1 and a%3==0)or (i-a==2)or(i+a==8):
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print()
+    
+
+
+
+
+
+
+
+
+
+'''
+*****
+*****
+*****
+*****
+     '''
+
+
+
+# k=1
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=5:
+#         print("*",end="")
+#         j=j+1
+#     print()
+#     i=i+1   
+
